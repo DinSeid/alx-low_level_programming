3a9e2f3ec05d07799e94de71ffcbaa509cee3512
DinSeid <dinznt@yahoo.com>
2022-12-13 06:53:19 -0800

    modified 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * times_table - prints the 9 times table, starting with 0
- */
+* times_table - prints the 9 times table, starting with 0
+*/
 void times_table(void)
 {
 int i, j, k;
@@ -28,7 +28,7 @@ _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
-_putchar('\n');
 }
+_putchar('\n');
 }
 }
