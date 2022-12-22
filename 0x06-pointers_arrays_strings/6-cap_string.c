1362b7f24da5363eec91d2746bdecd6f4ca00b53
DinSeid <dinznt@yahoo.com>
2022-12-22 03:39:16 -0800

    modified 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,11 +14,13 @@ char *cap_string(char *str)
 	while (str[index])
 
 	{
+
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 
 			index++;
 
 			if (str[index - 1] == ' ' ||
+
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
@@ -32,11 +34,14 @@ char *cap_string(char *str)
 				str[index - 1] == '{' ||
 				str[index - 1] == '}' ||
 				index == 0)
-
 				str[index] -= 32;
-				index++;
+
+		index++;
+
 	}
+
 	return (str);
+
 }
 
 
