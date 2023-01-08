f67dd22c65d36f01e4fc7d7fd010db08922f5c70
dinseid <dinzn@yahoo.com>
2023-01-08 00:08:31 -0800

    added the files of first task

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+/**
+ * _putchar- checks character
+ * @c: input value
+ * Return: char
+ */
+
+int _putchar(char c)
+
+{
+
+	return (putchar(c));
+
+}
