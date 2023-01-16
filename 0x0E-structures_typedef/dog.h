7e8215b1dcc983d7d86ff42ff4fb0c2fee2b871e
dinseid <dinzn@yahoo.com>
2023-01-16 12:13:37 -0800

    modified dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,28 +11,33 @@
  */
 
 struct dog
+
 {
-char *name;
-float age;
-char *owner;
+
+	char *name;
+
+	float age;
+
+	char *owner;
+
 };
 
-/**
- * dog_t - typedef for struct dog
- */
 
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
-void print_dog(struct dog *d);
+	typedef struct dog dog_t;
 
-dog_t *new_dog(char *name, float age, char *owner);
+	void init_dog(struct dog *d, char *name, float age, char *owner);
 
-void free_dog(dog_t *d);
+	void print_dog(struct dog *d);
 
-char *_strcpy(char *dest, char *src);
+	dog_t *new_dog(char *name, float age, char *owner);
 
-int _strlen(char *s);
+	void free_dog(dog_t *d);
+
+	char *_strcpy(char *dest, char *src);
+
+	int _strlen(char *s);
 
 #endif
+
 
